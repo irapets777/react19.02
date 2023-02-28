bde8bf676d1c717dd108a36684f9295e4898332f
Iryna Pec <118050604+irapets777@users.noreply.github.com>
2023-02-28 19:25:05 +0200

    color

--- src/components/PostCards.jsx
+++ src/components/PostCards.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-
 const PostCards = ({post}) => {
     const {title, body} = post;
   return (
     <div className="card m-3">
       <div className="card-body">
-        <h5 style={{color: "#f00", textTransform: "capitalize"}} className="card-title">{title}</h5>
+        <h5 style={{color: "#003d99", textTransform: "capitalize"}} className="card-title">{title}</h5>
         <p className="card-text">{body}</p>
+        <button >Delete</button>
+
       </div>
     </div>
   );
